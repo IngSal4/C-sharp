0ffce45d4c2ec23c143ec0b66d40222138c74cd0
IngSal4 <i.salinger@students.htl-leonding.ac.at>
2024-10-13 10:41:59 +0200

    Add files via upload

--- Praxis03/TimeAdder.cs
+++ Praxis03/TimeAdder.cs
@@ -8,50 +8,6 @@
  *--------------------------------------------------------------
 */
 
-
-
-
-using System;
-
-string input1
-string input2
-string input3
-string input4
-int hours1
-int minutes1
-int hours2
-int minutes2
-
-
-//E
-Console.Write("Einfacher Addierer für zwei Uhrzeiten ");
-Console.Write("===================================");
-Console.Write("Stunden 1 [int]: ");
-input1 = Console.ReadLine();
-hours1 = int.Parse(input1);
-Console.Write("Minuten 1 [int]: ");
-input2 = Console.ReadLine();
-minutes2 = int.Parse(input2);
-Console.Write("Stunden 2 [int]: ");
-input3 = Console.ReadLine();
-hours2 = int.Parse(input3);
-Console.Write("Minuten 2 [int]: ");
-input4 = Console.ReadLine();
-minutes2 = int.Parse(input4);
-
- 
-
-//V
-
-
-//zusammenrechnen 61 modulo 60 = 1
-
-/*überlauf von 60 minuten in 1 Stunde: 
-rechnet Minuten zusammen, 
-dividiert dann durch 60, 
-dann erhält man die vollen Stunden(=Ergebnis) 
-und diese addiert man zur Summe dazu.*/
-
 using System;
 
 string input1;
@@ -90,14 +46,8 @@ extraHours = totalMin / 60;
 totalHours = hours1 + hours2 + extraHours;
 minutes    = totalMin % 60;
 
-//A
-/* 
-  20:03
-+  1:05
-= 21:08
-*/
+Console.WriteLine($"{hours1,8}:{minutes1:00}");
+Console.WriteLine($"+{hours2,7}:{minutes2:00}");
+Console.WriteLine($"={totalHours,7}:{minutes:00}");
 
-Console.WriteLine($"{hours1:00,}{minutes1:00}");
-Console.WriteLine($"{hours2:00,}{minutes2:00}");
-Console.WriteLine($"{totalHours:00}{minutes:00}");
-
+Console.ReadLine();
